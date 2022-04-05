c78db189fb14bb0a1420719e8a3bfbad5ada4ac4
srinidhi2 <gantasrinidhi@gmail.com>
2022-04-05 15:53:22 +0530

    changes done to diff.java

--- Diff.java
+++ Diff.java
@@ -1,7 +1,8 @@
 public class Diff{
     public static void main(String[]args){
         int a=5;
-        int b=6;
+        int b=6,c=9,d=3;
         System.out.println("The diff is "+ (b-a));
+        System.out.println("The diff is "+ (c-d));
     }
 }
